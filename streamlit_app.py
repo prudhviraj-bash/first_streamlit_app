9e60902b179580277f37e442071c04993b660f4d
prudhviraj-bash <56086861+prudhviraj-bash@users.noreply.github.com>
2022-09-21 20:11:17 +0530

    Update streamlit_app.py
    Added Extra items

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.title('My Parents New Healthy  Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Oat Meal & Egg Omlette')
+streamlit.text('Kiwi juice - with Raisins')
+streamlit.text('Chicken Biryani with Tender Chicken Kabab')
