36af8387eae521bcce4db35e8df28a78e9580670
prudhviraj-bash <56086861+prudhviraj-bash@users.noreply.github.com>
2022-09-25 16:23:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ try:
        back_from_function = get_fruityvice_data(fruit_choice)
        streamlit.dataframe(back_from_function) 
    #except URLError as e:
-    streamlit.error()
+   #streamlit.error()
                   
 streamlit.write('The user entered ', fruit_choice)
 #import requests
